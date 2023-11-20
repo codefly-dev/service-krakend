2109376751d1ef979794181c5c59d21e15794475
Antoine Toussaint <antoine@antoines-mbp-2.myfiosgateway.com>
2023-11-20 10:03:16 +0100

    release: 0.0.1

--- build.sh
+++ build.sh
@@ -1,2 +1,5 @@
-echo Building krakend
-go build -o ~/.codefly/plugins/services/codefly.ai/krakend:latest *.go
+PLUGIN=$( yq e '.name' plugin.codefly.yaml)
+VERSION=$(yq e '.version' plugin.codefly.yaml
+)
+echo Building ${PLUGIN}:${VERSION}
+go build -o ~/.codefly/plugins/services/codefly.ai/${PLUGIN}:${VERSION} *.go
